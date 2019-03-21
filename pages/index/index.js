9e63a6f2a1ad549bedb567e542f51a3153831861
jiangyukai <jiangyukai>
2019-03-21 14:19:32 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,20 @@
+Page({
+
+  data: {
+    loading: false,
+    loadtxt: '正在加载',
+    currentId: '1001',
+    banners: [],
+    RollLink: [],
+    AvatorText: [],
+    newList: [],
+    section: [
+      { name: '精选', id: '1001' }, { name: '黄金单身汉', id: '1032' },
+      { name: '综艺', id: '1003' }, { name: '电视剧', id: '1004' },
+      { name: '电影', id: '1005' }, { name: '少儿', id: '1021' }
+    ]
+  }
+
+
+    
+})
