93b43b7bfe907c0f54ae0c40b45dc42d611c3bf3
jiangyukai <jiangyukai>
2019-03-25 18:57:00 +0800

    新增轮播

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,15 +4,28 @@ Page({
     loading: false,
     loadtxt: '正在加载',
     currentId: '1001',
-    banners: [],
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
     RollLink: [],
     AvatorText: [],
     newList: [],
     section: [
-      { name: '精选', id: '1001' }, { name: '黄金单身汉', id: '1032' },
-      { name: '综艺', id: '1003' }, { name: '电视剧', id: '1004' },
-      { name: '电影', id: '1005' }, { name: '少儿', id: '1021' }
+      { name: '时政', id: '1001' }, { name: '解读', id: '1032' },
+      { name: '视点', id: '1003' }, { name: '交易', id: '1004' },
+      { name: '质价标准', id: '1005' }, { name: '质价标准', id: '1006' },
+      { name: '质价标准', id: '1007' }, { name: '质价标准', id: '1008' }
     ]
+  },
+
+  handleTap: function (e) {
+
+    console.log(e);
+    let id = e.currentTarget.id;
+
+    if (id) {
+      this.setData({ currentId: id })
+      this.onLoad();
+    }
+
   }
 
 
