0763f15870baaf314362d4fb45bcf1adea93c661
jiangyukai <jiangyukai>
2019-03-25 10:43:08 +0800

    测试提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,7 +9,7 @@ Page({
     AvatorText: [],
     newList: [],
     section: [
-      { name: '精11选', id: '1001' }, { name: '黄金单身汉', id: '1032' },
+      { name: '精选', id: '1001' }, { name: '黄金单身汉', id: '1032' },
       { name: '综艺', id: '1003' }, { name: '电视剧', id: '1004' },
       { name: '电影', id: '1005' }, { name: '少儿', id: '1021' }
     ]
